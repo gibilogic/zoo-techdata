39af581c1279b815df6e4167a0de4982c90b101b
zanardi <f.abeni@gibilogic.com>
2014-11-25 21:42:57 +0100

    Add basic edit style

--- src/tmpl/edit.php
+++ src/tmpl/edit.php
@@ -1,19 +1,24 @@
 <?php
 
 /**
- * @version     tmpl/edit.php 2013-07-09 09:49:00Z zanardi
+ * @version     tmpl/edit.php 2014-11-25 09:40:00 UTC zanardi
  * @package     Techdata element for Zoo
  * @author      GiBiLogic <info@gibilogic.com>
  * @authorUrl   http://www.gibilogic.com
- * @copyright   (C) 2013 GiBiLogic snc
- * @license     GNU/GPL v2 or later
+ * @copyright   (C) 2013-2014 GiBiLogic snc
+ * @license     GNU/GPL v3 or later
  */
 defined('_JEXEC') or die();
 
 $fields = array_map("trim",explode(",",$this->config->get('fields')));
 ?>
-<div>
+<style type="text/css">
+  div.element div.techdata-edit label {clear: left;display: block;float: left;width: 50%;text-align: right}
+  div.element div.techdata-edit input {float: left;margin-left: 2%;width: 38%}
+</style>
+<div class="techdata-edit">
 <?php foreach ($fields as $field): ?>
+    <?php $field = str_replace('*','',$field); ?>
     <?php $field_lang_key = strtoupper(str_replace(" ","_",$field)); ?>
     <label><?php echo JText::_("ZOO_TECHDATA_$field_lang_key") ?></label><?php echo $this->app->html->_('control.text', $this->getControlName($field), $this->get($field), 'title="'.JText::_('Length').'" placeholder="'.JText::_($field).'"'); ?><br />
 <?php endforeach ?>
